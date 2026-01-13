9dc75d0c4ad23ef2fbe7126a8b111556bdb9bf95
zebrapurring <>
2026-01-13 23:11:44 +0100

    fix: crash calling concatenate_array3 with empty arrays

--- src/helpers/scrfd_helper.rs
+++ src/helpers/scrfd_helper.rs
@@ -385,11 +385,7 @@ impl ScrfdHelpers {
     /// ```
     pub fn concatenate_array3(arrays: &[Array3<f32>]) -> Result<Array3<f32>, Box<dyn Error>> {
         if arrays.is_empty() {
-            return Ok(Array3::<f32>::zeros((
-                0,
-                arrays[0].shape()[1],
-                arrays[0].shape()[2],
-            )));
+            return Ok(Array3::<f32>::zeros((0, 0, 0)));
         }
         Ok(ndarray::concatenate(
             Axis(0),
